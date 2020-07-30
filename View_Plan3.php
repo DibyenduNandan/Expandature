2483301d4c4fe9de915e9d3623f9f6fd9e230c68
Dibyendu <nandandibyendu31@gmail.com>
2020-07-30 23:15:48 +0530

    Deploy

--- View_Plan3.php
+++ View_Plan3.php
@@ -64,10 +64,9 @@ if (isset($_POST['submit']))
             $ext= GetImageExtension($imgtype);
             $imagename=date("d-m-Y")."-".time().$ext; 
             $target_path = "image/".$imagename; 
+            die(echo 1;);
             if(move_uploaded_file($temp_name, $target_path) && $ext!=false)
             {
-                echo 1;
-                exit(1);
                 $sql='UPDATE `people` SET `title` = "'.$title.'",`bills` = "'.$target_path.'",`amount` = '.$spent.',`date_time` = "'.$date.'" WHERE `person_name`="'.$user.'" and `title`="No title"';
                 $result=mysqli_query($conn,$sql);
                 if($result){
